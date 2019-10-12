0e2acf001dfe26ba97845b1ff0e5f6b66d7bf319
antonmry <anton@galiglobal.com>
2019-10-12 22:26:18 +0200

    Grains finished

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -3,11 +3,20 @@ import java.math.BigInteger;
 class Grains {
 
     BigInteger computeNumberOfGrainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if ((square < 1) || (square > 64)) throw new IllegalArgumentException("square must be between 1 and 64");
+        return BigInteger.valueOf(2).pow(square - 1);
+    }
+
+    BigInteger totalRecursive(final int square) {
+        if (square == 1) {
+            return BigInteger.valueOf(2).pow(square - 1);
+        }
+
+        return BigInteger.valueOf(2).pow(square - 1).add(totalRecursive(square - 1));
     }
 
     BigInteger computeTotalNumberOfGrainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return totalRecursive(64);
     }
 
 }
