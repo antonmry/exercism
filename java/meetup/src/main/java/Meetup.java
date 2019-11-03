ab933f6752cbae3050362b955c6eabbc17c2a60e
antonmry <anton@galiglobal.com>
2019-11-03 23:01:02 +0100

    Solved meetup java

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,10 +1,36 @@
-/*
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+import static java.time.temporal.TemporalAdjusters.*;
 
-Please remove this comment when submitting your solution.
 
-*/
+public class Meetup {
+    private final int year;
+    private final int month;
+
+    public Meetup(int month, int year) {
+        this.month = month;
+        this.year = year;
+    }
+
+    public LocalDate day(DayOfWeek day, MeetupSchedule schedule) {
+        LocalDate ld = LocalDate.now().withYear(year).withMonth(month);
+        return switch (schedule) {
+            case FIRST -> ld.with(firstInMonth(day));
+            case SECOND -> ld.with(firstInMonth(day)).with(next(day));
+            case THIRD -> ld.with(firstInMonth(day)).with(next(day)).with(next(day));
+            case FOURTH -> ld.with(firstInMonth(day)).with(next(day)).with(next(day)).with(next(day));
+            case LAST -> ld.with(lastInMonth(day));
+            case TEENTH -> calculateTeenth(day, ld);
+            default -> throw new UnsupportedOperationException();
+        };
+    }
+
+    private LocalDate calculateTeenth(DayOfWeek day, LocalDate ld) {
+        int d = ld.with(firstInMonth(day)).with(next(day)).with(next(day)).getDayOfMonth();
+        if (d > 9 && d < 20)
+            return ld.with(firstInMonth(day)).with(next(day)).with(next(day));
+        else
+            return ld.with(firstInMonth(day)).with(next(day));
+    }
+}
