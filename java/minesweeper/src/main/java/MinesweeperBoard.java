f55aa434c7a488747a63fae1e0277adf7bb1bef7
antonmry <anton@galiglobal.com>
2020-03-10 19:28:51 +0100

    Minesweeper improved

--- java/minesweeper/src/main/java/MinesweeperBoard.java
+++ java/minesweeper/src/main/java/MinesweeperBoard.java
@@ -1,55 +1,44 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MinesweeperBoard {
     char[][] board;
 
     public MinesweeperBoard(List<String> inputBoard) {
-        if (inputBoard.size() > 0) {
-            board = new char[inputBoard.size()][inputBoard.get(0).length()];
-        } else {
-            board = new char[inputBoard.size()][0];
-        }
-        for (int i = 0; i < inputBoard.size(); i++) {
-            for (int j = 0; j < inputBoard.get(0).length(); j++) {
-                board[i][j] = inputBoard.get(i).charAt(j);
-            }
-        }
+        this.board = inputBoard.stream()
+                .map(s -> s.toCharArray())
+                .toArray(char[][]::new);
     }
 
-    boolean isBomb(int x, int y) {
-        if (y > board.length - 1 || y < 0) return false;
-        if (x > board[y].length - 1 || x < 0) return false;
-        return board[y][x] == '*';
+    public List<String> withNumbers() {
+
+        return IntStream.range(0, board.length)
+                .mapToObj(y -> IntStream.range(0, board[y].length)
+                        .mapToObj(x -> calculateCell(x, y))
+                        .collect(StringBuilder::new,
+                                StringBuilder::append,
+                                StringBuilder::append)
+                        .toString())
+                .collect(Collectors.toList());
     }
 
     char calculateCell(int x, int y) {
         if (board[y][x] == '*') return board[y][x];
 
         int hint = 0;
-        for (int a = -1; a < 2; a++) {
-            for (int b = -1; b < 2; b++) {
-                if (isBomb(x + b, y + a)) hint++;
-            }
-        }
+        for (int i = -1; i < 2; i++)
+            for (int j = -1; j < 2; j++)
+                if (isBomb(x + j, y + i)) hint++;
 
         if (hint == 0) return board[y][x];
         return String.valueOf(hint).charAt(0);
     }
 
-    public List<String> withNumbers() {
-
-        List<String> result = new ArrayList<>();
-
-        for (int y = 0; y < board.length; y++) {
-            char[] line = new char[board[y].length];
-            for (int x = 0; x < board[y].length; x++) {
-                line[x] = calculateCell(x, y);
-            }
-            result.add(String.valueOf(line));
-        }
-
-        return result;
+    boolean isBomb(int x, int y) {
+        if (y > board.length - 1 || y < 0) return false;
+        if (x > board[y].length - 1 || x < 0) return false;
+        return board[y][x] == '*';
     }
 
 }
