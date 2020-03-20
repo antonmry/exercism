79365e818025b181560740fc03616487003af015
antonmry <anton@galiglobal.com>
2020-03-20 21:44:25 +0100

    Scala two-fer: updated with mentor feedback

--- scala/two-fer/src/main/scala/Twofer.scala
+++ scala/two-fer/src/main/scala/Twofer.scala
@@ -1,5 +1,3 @@
 object Twofer {
-  def twofer(): String = twofer("you")
-
-  def twofer(name: String): String = String.format("One for %1$s, one for me.", name)
+  def twofer(name: String = "you"): String = s"One for $name, one for me."
 }
