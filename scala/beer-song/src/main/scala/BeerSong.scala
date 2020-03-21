45bdb2a375a615251beae761a957f06dd736d726
antonmry <anton@galiglobal.com>
2020-03-21 22:59:44 +0100

    scala beer-song refactored

--- scala/beer-song/src/main/scala/BeerSong.scala
+++ scala/beer-song/src/main/scala/BeerSong.scala
@@ -1,16 +1,16 @@
 object BeerSong {
-  def reciteVerse(verse: Int): String = verse match {
+  private val verse: Int => String = {
     case 0 =>
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     case 1 =>
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     case 2 =>
       "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    case _ => s"$verse bottles of beer on the wall, $verse bottles of beer.\n" +
+    case verse => s"$verse bottles of beer on the wall, $verse bottles of beer.\n" +
       s"Take one down and pass it around, ${verse - 1} bottles of beer on the wall.\n"
   }
 
-  def recite(verse: Int, numberOfVerses: Int): String = {
-    ((verse - numberOfVerses + 1) to verse).reverse.map(reciteVerse(_)).reduce(_ + "\n" + _)
+  def recite(firstVerse: Int, numberOfVerses: Int): String = {
+    (firstVerse until (firstVerse - numberOfVerses) by -1).map(verse).reduce(_ + "\n" + _)
   }
 }
