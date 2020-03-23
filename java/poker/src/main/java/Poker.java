017b1b0b5f7d23de48bc24d6daa2cbb615aaa7b0
antonmry <anton@galiglobal.com>
2020-03-23 21:17:36 +0100

    java Poker implementation without pairs

--- java/poker/src/main/java/Poker.java
+++ java/poker/src/main/java/Poker.java
@@ -1,5 +1,9 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * Simple card: x 1
@@ -10,14 +14,33 @@ import java.util.List;
 
 public class Poker {
 
-
-    private List<String> hands;
+    private HashMap<String, Long> hands = new HashMap<>();
+    private long maxValue = 0;
 
     public Poker(List<String> hands) {
-        this.hands = hands;
+
+        hands.forEach(hand -> {
+            List<Character> cardsValues = Arrays.stream(hand.split(" "))
+                    .map(s -> (s.charAt(0)))
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            long handValue = IntStream.range(0, hand.split(" ").length)
+                    .mapToLong(cardPos -> cardsValues.get(cardPos) * (long) Math.pow(100, cardPos))
+                    .sum();
+
+            System.out.println(hand + ": " + handValue);
+            this.hands.put(hand, handValue);
+            if (handValue > maxValue) {
+                maxValue = handValue;
+            }
+        });
     }
 
     public List<String> getBestHands() {
-        return hands;
+        return hands.entrySet().stream()
+                .filter(hand -> hand.getValue() == maxValue)
+                .map(hand -> hand.getKey())
+                .collect(Collectors.toList());
     }
 }
