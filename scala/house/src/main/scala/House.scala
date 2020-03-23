c8f1f835a4c717189803af408b80efe1ac5c7f76
antonmry <anton@galiglobal.com>
2020-03-23 22:03:06 +0100

    scala house solved

--- scala/house/src/main/scala/House.scala
+++ scala/house/src/main/scala/House.scala
@@ -1,4 +1,21 @@
 object House {
-  def recite(i: Int, i1: Int): String = ""
 
+  val names = List("house that Jack built", "malt", "rat", "cat", "dog",
+    "cow with the crumpled horn", "maiden all forlorn", "man all tattered and torn",
+    "priest all shaven and shorn", "rooster that crowed in the morn", "farmer sowing his corn",
+    "horse and the hound and the horn")
+
+  val verbs = List("lay in", "ate", "killed", "worried", "tossed", "milked", "kissed",
+    "married", "woke", "kept", "belonged to")
+
+  def reciteVerse(verse: Int): String =
+    "This is the " + names(verse - 1) +
+      (verse - 1 until 0 by -1)
+        .map(i => s" that ${verbs(i - 1)} the ${names(i - 1)}")
+        .mkString("") +
+      ".\n"
+
+  def recite(firstVerse: Int, lastVerse: Int): String =
+    (firstVerse to lastVerse).map(reciteVerse).mkString("") + "\n"
 }
+
