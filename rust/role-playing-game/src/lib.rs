c247cf9559b9e7483fa07f095243db410a8cd3fb
Anton Rodriguez <anton@galiglobal.com>
2022-11-09 18:52:57 +0100

    Rust: solved role playing game

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -1,7 +1,3 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub struct Player {
     pub health: u32,
     pub mana: Option<u32>,
@@ -9,11 +5,43 @@ pub struct Player {
 }
 
 impl Player {
+    // Revive this player
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health != 0 {
+            return None;
+        }
+
+        if self.level >= 10 {
+            return Some(Player {
+                health: 100,
+                mana: Some(100),
+                level: self.level,
+            });
+        } else {
+            return Some(Player {
+                health: 100,
+                mana: None,
+                level: self.level,
+            });
+        }
     }
 
+    // Cast a spell of cost mana_cost
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        if self.mana.is_none() {
+            self.health = if self.health < mana_cost {
+                0
+            } else {
+                self.health - mana_cost
+            };
+            return 0;
+        }
+
+        if self.mana.unwrap() < mana_cost {
+            return 0;
+        };
+
+        self.mana = Some(self.mana.unwrap() - mana_cost);
+        return 2 * mana_cost;
     }
 }
