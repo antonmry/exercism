69166e1be9ec6f50fcd7439151b53721de209538
Anton Rodriguez <anton@galiglobal.com>
2022-11-14 20:29:53 +0100

    Rust: rpn-calculator solved

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -8,8 +8,30 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut stack: Vec<i32> = Vec::new();
+
+    for n in inputs {
+        let temp = match n {
+            CalculatorInput::Add if stack.len() >= 2 => stack.pop().unwrap() + stack.pop().unwrap(),
+            CalculatorInput::Subtract if stack.len() >= 2 => {
+                -1 * stack.pop().unwrap() + stack.pop().unwrap()
+            }
+            CalculatorInput::Multiply if stack.len() >= 2 => {
+                stack.pop().unwrap() * stack.pop().unwrap()
+            }
+            CalculatorInput::Divide if stack.len() >= 2 => {
+                let t = stack.pop().unwrap();
+                stack.pop().unwrap() / t
+            }
+            CalculatorInput::Value(n) => *n,
+            _ => return None,
+        };
+
+        stack.push(temp);
+    }
+
+    match stack.len() {
+        1 => stack.pop(),
+        _ => None,
+    }
 }
