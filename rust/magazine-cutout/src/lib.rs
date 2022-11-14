bdf6109237fa6b7c7cefaca7a98fdef95adbd011
Anton Rodriguez <anton@galiglobal.com>
2022-11-14 11:46:38 +0100

    Magazine cutout: refactor

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,24 +1,16 @@
 use std::collections::HashMap;
 
-//fn calculate_occurrences(text: &[&str]) -> HashMap<&str, i32> {
-//    text.iter().fold(HashMap::new(), |mut map, c| {
-//        *map.entry(c).or_insert(0) += 1;
-//        map
-//    })
-//}
+fn calculate_occurrences<'a>(text: &[&'a str]) -> HashMap<&'a str, i32> {
+    text.iter().fold(HashMap::new(), |mut map, c| {
+        *map.entry(c).or_insert(0) += 1;
+        map
+    })
+}
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    let magazine_counts: HashMap<&str, i32> = magazine.iter().fold(HashMap::new(), |mut map, c| {
-        *map.entry(c).or_insert(0) += 1;
-        map
-    });
+    let magazine_counts = calculate_occurrences(magazine);
 
-    let note_counts: HashMap<&str, i32> = note.iter().fold(HashMap::new(), |mut map, c| {
-        *map.entry(c).or_insert(0) += 1;
-        map
-    });
-
-    note_counts
+    calculate_occurrences(note)
         .iter()
         .all(|e| magazine_counts.get(e.0).unwrap_or(&0) >= e.1)
 }
